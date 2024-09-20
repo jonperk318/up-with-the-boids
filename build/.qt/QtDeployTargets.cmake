efe1967925a67e394e9b4af0c0becf7080a9fe14
Jonathan Perkins <jperkin4@uab.edu>
2024-09-19 23:08:26 -0500

    Messing around

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_example_FILE /Users/jp/Documents/GitHub.nosync/up-with-the-boids/build/example)
+set(__QT_DEPLOY_TARGET_example_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/example.dir/MainWindow.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/example.dir/MainWindow.cpp.o: \
+  /Users/jp/Documents/GitHub.nosync/up-with-the-boids/MainWindow.cpp
